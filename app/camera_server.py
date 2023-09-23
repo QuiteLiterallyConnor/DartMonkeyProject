4148f490e59ecc621816e55c059b58d44f97987d
QuiteLiterallyConnor <628cjh@gmail.com>
2023-09-22 21:46:27 -0500

    dev

--- app/camera_server.py
+++ app/camera_server.py
@@ -1,26 +1,39 @@
 from flask import Flask, Response
+from vidgear.gears import VideoGear
 import cv2
-import sys
 
 app = Flask(__name__)
 
 # Open the webcam
-video_capture = cv2.VideoCapture(1)
+stream = VideoGear(source=1).start()
 
 def generate_frames():
     while True:
-        success, frame = video_capture.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        frame = stream.read()
+        if frame is None:
+            continue
+        # Encode frame as JPEG
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/stream')
+@app.route('/video')
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(host='192.168.0.106', port=sys.argv[1])
+@app.route('/')
+def index():
+    return """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Webcam Stream</title>
+</head>
+<body>
+    <img src="/video" width="640" height="480" />
+</body>
+</html>"""
+
+if __name__ == "__main__":
+    app.run(port=sys.argv[1])
