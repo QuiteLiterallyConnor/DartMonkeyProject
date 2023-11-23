2ccc601186fb25f7b32726963cfb1c61db0c54e8
QuiteLiterallyConnor <628cjh@gmail.com>
2023-11-23 12:20:14 -0600

    dev

--- test_sql/main.go
+++ test_sql/main.go
@@ -20,12 +20,37 @@ type Token struct {
 }
 
 func main() {
-	// Retrieve environment variables
+	db := connectDb("tokens")
+
+	// Generate phony data
+	// tokens := []Token{
+	// 	{Created_Time: time.Now(), Used_Time: time.Now(), TokenID: "token1", IsUsed: false},
+	// 	{Created_Time: time.Now(), Used_Time: time.Now(), TokenID: "token2", IsUsed: false},
+	// }
+
+	// // Insert tokens into the database
+	// for _, token := range tokens {
+	// 	result := db.Create(&token)
+	// 	if result.Error != nil {
+	// 		log.Fatal(result.Error)
+	// 	}
+	// }
+
+	// log.Println("Tokens inserted into the database successfully.")
+
+	// Example usage of removeTokens function
+	removeTokens(db, "token1")
+
+	// Retrieve and print tokens from the database
+	printTokens(db)
+}
+
+func connectDb(dbName string) *gorm.DB {
 	user := os.Getenv("SQL_USER")
 	pass := os.Getenv("SQL_PASSWORD")
 
 	// Set up database connection string
-	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/tokens?charset=utf8mb4&parseTime=True&loc=Local", user, pass)
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, dbName)
 
 	// Open database connection with GORM
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -36,27 +61,7 @@ func main() {
 	// AutoMigrate the Token struct
 	db.AutoMigrate(&Token{})
 
-	// Generate phony data
-	tokens := []Token{
-		{Created_Time: time.Now(), Used_Time: time.Now(), TokenID: "token1", IsUsed: false},
-		{Created_Time: time.Now(), Used_Time: time.Now(), TokenID: "token2", IsUsed: false},
-	}
-
-	// Insert tokens into the database
-	for _, token := range tokens {
-		result := db.Create(&token)
-		if result.Error != nil {
-			log.Fatal(result.Error)
-		}
-	}
-
-	log.Println("Tokens inserted into the database successfully.")
-
-	// Example usage of removeTokens function
-	removeTokens(db, "token1")
-
-	// Retrieve and print tokens from the database
-	printTokens(db)
+	return db
 }
 
 // removeTokens removes all entries with a specified TokenID
